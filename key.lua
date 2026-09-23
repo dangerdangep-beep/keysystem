63962d414e0d8881f47bc89df743384d1ddea820
dangerdangep-beep <dangerdangep@gmail.com>
2026-09-23 17:34:28 +0300

    key.lua

--- key.lua
+++ key.lua
@@ -1,116 +1,48 @@
--- Protected by Mnx Obfuscator | Public Enemy
-local k1O21nXTrrQp=type(nil)
-local a8eTusjKs5ON=#"yT4jq0"
-local mzEcuRnn3o=bit32.bxor(153,143)
-local baP3mrJRl44cBh=math.abs(-59)
-local sbBa8DRrJCMlkvugki=type(nil)
-local wqvlk1WFLV=type(nil)
-local u7QPGZYEX2aW=string.len("pssi6L8ZxQ")
-local xnNEdeqc1ljRE=math.floor(0)
-local zP6UiA4akjr=math.abs(-76)
-local aPBOHTGW5CKEh6iZ=tostring(98)
-local jCN7aK7OsjY=#"daB"
-local vnucPKOdv0qPR=837
-do local _=nil end
-do local _=nil end
-local ynvI9CCM7f=type(nil)
-local m24F_u5yQySmow1i=type(nil)
-for _=1,0 do end
-if false then error("") end
-if false then error("") end
-local feVCjzieWs2PJn={"f7630d1adec10f3f6867f0e692328554e74eed2a14e657e2f56d86ce0f686dc80fbc76a4bed366770a3bbfa3d0379d","e258c956e69ed3d4d5dc098f49b1074aa48cd8240cd4c79a2d4f19eabbab93a8bed8fad07dafd2b211a3fa2f7928039e","694bb576421d572203ccb7f405feca7843028db55078e2affbfa74694b789754bbf7e8aab74be8be2ef9432e54f25161","a1e62703309c09d3877200fd6d77396ca54afe82e9520a6643969c121fd1c8b38a08452f7d5dff863319878f8882bbe5","3cdade77077d42fc62e9b36ed0cc16884a09703ab447c644ac2b02643189b2f1e7a3f5812ca5ae873fff46bc3fb0d68b","63ae6b2c4f3b0991b18d923d8d859116991e123be1fd044b9b0ac79ff95fe79615caea7f713dafafda9c125a7e56f808","b6c8adf12a92a925affd3d425f8b45529d14274d2ab08ccad07c5146ba0be2743c22a3f7449f44b0410b3799faa19d1d","e78de4c38f822b553952e5cef20722774fda6c2c001aaa1402f0fd733a4dfbcfe7af3d42249be37e1a0f44c7cc5140aa"}
-local yiG1zPJjq={8,7,6,4,1,5,2,3}
-local gpi1hUkz_BG580Z={"46b74b7b5eeee832ab09643ca205b658a4c2402fb1989e87d23935aa887b0c616692c7f0c839a661334ddb1227f02146","1163971ee30bfa23d73df28115828033c5de63a82aa22b4b817a334df500fa644c47869a6ed0fa47cadda2e497941dfc","3523a27a19346a476d7ade757c42aa89f00781fa3a6995390f1fcb04b5bd95d6a01e84c1288de76810539259f93c9430","1417abe0f4803e5eec95f4addf6cfc3349e801c084800f4151c73b43a03a3c5fdc24963e324bb25178b52616cbef60d2","2480a34d631e4aed99f10a5a6fe2746d0aa673e80e3cf80b7c8a112cd761ec64a8c40bdefec2986bdab78cb34ee61695","c4ea19bdb1ceaa051cb04e9d34706e059d4038b092a105e2c5ba36685b8bcdb3fd8280298357457009df2fb83abff4","6f998dfbc8090c6bd307d5784f13c292f0c8f2aadee07f9df99cc6a5813353b3262e707a5cd305fa27b1056b81a2c8dd","38cb2ed02a3ae8f2c59c201f0ae861779caa13cb9b2b4ab187ee7e717f3eacf6b00e86c01b1c6f9557f8919a1355ca67"}
-local rYK9S7lOQq6B={6,7,1,4,2,8,5,3}
-local qCAG7RZST7SBgP=type(nil)
-local ohtZ7NYPC9gG8ZN5=math.abs(-54)
-local cHV08EgvS=type(nil)
-local yOwuxgHCzztPLR=bit32.bxor(150,183)
-for _=1,0 do end
-if false then error("") end
-local zJYb_2Bv6LZSX={}
-for zY6yF5hWPp7u=1,#yiG1zPJjq do zJYb_2Bv6LZSX[yiG1zPJjq[zY6yF5hWPp7u]]=feVCjzieWs2PJn[zY6yF5hWPp7u] end
-zJYb_2Bv6LZSX=table.concat(zJYb_2Bv6LZSX)
-local h_9mzJesuj={}
-for zY6yF5hWPp7u=1,#rYK9S7lOQq6B do h_9mzJesuj[rYK9S7lOQq6B[zY6yF5hWPp7u]]=gpi1hUkz_BG580Z[zY6yF5hWPp7u] end
-h_9mzJesuj=table.concat(h_9mzJesuj)
-local kT1qPS81plsPCGX={}
-for zY6yF5hWPp7u=1,#zJYb_2Bv6LZSX,2 do
-  kT1qPS81plsPCGX[#kT1qPS81plsPCGX+1]=tonumber(string.sub(zJYb_2Bv6LZSX,zY6yF5hWPp7u,zY6yF5hWPp7u+1),16)
-end
-local zPclFqd8IseupNqxmE={62,194,117,195,2,143,52,14,205,41,171,251,181,19,80,81,179,232,189,77,176,162,71,9,58,179,89,108,129,209,154,166,232,148,176,96,97,107}
-for zY6yF5hWPp7u=1,#kT1qPS81plsPCGX do
-  kT1qPS81plsPCGX[zY6yF5hWPp7u]=bit32.bxor(kT1qPS81plsPCGX[zY6yF5hWPp7u],zPclFqd8IseupNqxmE[(zY6yF5hWPp7u-1)%#zPclFqd8IseupNqxmE+1])
-end
-local gteY8yPNGVYR0z98h3={}
-for zY6yF5hWPp7u=1,#h_9mzJesuj,2 do
-  gteY8yPNGVYR0z98h3[#gteY8yPNGVYR0z98h3+1]=tonumber(string.sub(h_9mzJesuj,zY6yF5hWPp7u,zY6yF5hWPp7u+1),16)
-end
-local mxhTpiDWHxbQDB9Hfa={67,117,33,159,100,99,178,70,187,176,92,178,5,164,249,64,188,12,84,63,10,40,200,109,190,79,62,91,120,183,7,53,117,207,41,21,112,85}
-for zY6yF5hWPp7u=1,#gteY8yPNGVYR0z98h3 do
-  gteY8yPNGVYR0z98h3[zY6yF5hWPp7u]=bit32.bxor(gteY8yPNGVYR0z98h3[zY6yF5hWPp7u],mxhTpiDWHxbQDB9Hfa[(zY6yF5hWPp7u-1)%#mxhTpiDWHxbQDB9Hfa+1])
-end
-local oLqrgMicI={}
-for zY6yF5hWPp7u=1,math.max(#kT1qPS81plsPCGX,#gteY8yPNGVYR0z98h3) do
-  if kT1qPS81plsPCGX[zY6yF5hWPp7u] then oLqrgMicI[#oLqrgMicI+1]=kT1qPS81plsPCGX[zY6yF5hWPp7u] end
-  if gteY8yPNGVYR0z98h3[zY6yF5hWPp7u] then oLqrgMicI[#oLqrgMicI+1]=gteY8yPNGVYR0z98h3[zY6yF5hWPp7u] end
-end
-local tSm9rmiPjW8={}
-local ggMLDIqxo7Dz3h984f=98461015
-for zY6yF5hWPp7u=#oLqrgMicI-1,1,-1 do
-  ggMLDIqxo7Dz3h984f=(ggMLDIqxo7Dz3h984f*1664525+1013904223)%4294967296
-  local jQSGL5H3DckMGe9gQ=ggMLDIqxo7Dz3h984f%(zY6yF5hWPp7u+1)+1
-  tSm9rmiPjW8[#tSm9rmiPjW8+1]={zY6yF5hWPp7u+1,jQSGL5H3DckMGe9gQ}
-end
-for zY6yF5hWPp7u=#tSm9rmiPjW8,1,-1 do
-  local pxV4KhPsuc7NhKA3Jk=tSm9rmiPjW8[zY6yF5hWPp7u]
-  local xDQ_yLjDOqyCogH9z=oLqrgMicI[pxV4KhPsuc7NhKA3Jk[1]]
-  oLqrgMicI[pxV4KhPsuc7NhKA3Jk[1]]=oLqrgMicI[pxV4KhPsuc7NhKA3Jk[2]]
-  oLqrgMicI[pxV4KhPsuc7NhKA3Jk[2]]=xDQ_yLjDOqyCogH9z
-end
-local k5zRX4M95bnX5V={}
-local h66z3cH7GI=888793461
-for zY6yF5hWPp7u=#oLqrgMicI-1,1,-1 do
-  h66z3cH7GI=(h66z3cH7GI*1664525+1013904223)%4294967296
-  local l2qAERtIa=h66z3cH7GI%(zY6yF5hWPp7u+1)+1
-  k5zRX4M95bnX5V[#k5zRX4M95bnX5V+1]={zY6yF5hWPp7u+1,l2qAERtIa}
-end
-for zY6yF5hWPp7u=#k5zRX4M95bnX5V,1,-1 do
-  local yh1z3yACP=k5zRX4M95bnX5V[zY6yF5hWPp7u]
-  local maiUmrEDaAQ=oLqrgMicI[yh1z3yACP[1]]
-  oLqrgMicI[yh1z3yACP[1]]=oLqrgMicI[yh1z3yACP[2]]
-  oLqrgMicI[yh1z3yACP[2]]=maiUmrEDaAQ
-end
-local pKyaAfbhuA={132,36,62,105,107,103,134,81,194,113,57,127,160,72,152,250,94,196,35,128,81,113,4,215,131,249,142,33,50,192,148,242,156,107,80,8,163,223}
-for zY6yF5hWPp7u=1,#oLqrgMicI do
-  oLqrgMicI[zY6yF5hWPp7u]=bit32.bxor(oLqrgMicI[zY6yF5hWPp7u],pKyaAfbhuA[(zY6yF5hWPp7u-1)%#pKyaAfbhuA+1])
-end
-local lWsgE501NUmuIN2={173,150,111,99,244,167,114,250,144,230,53,176,249,8,90,52,250,163,74,192,167,208,40,39,107,61,169,75,162,90,119,146,225,220,239}
-for zY6yF5hWPp7u=1,#oLqrgMicI do
-  oLqrgMicI[zY6yF5hWPp7u]=bit32.bxor(oLqrgMicI[zY6yF5hWPp7u],lWsgE501NUmuIN2[(zY6yF5hWPp7u-1)%#lWsgE501NUmuIN2+1])
-end
-local leU_9U6Kv=math.floor(4)
-local u9OrNVeNSFcmGsl6=#"gIvx"
-local aK7uu2QHhFmzF=bit32.bxor(47,190)
-local o_vfourxdxV8=type(nil)
-local g0204nntpQ1d=tostring(63)
-local yWNsQYSnwjYHiv=#"x0pyREJ"
-local kwoIohWVHAP=type(nil)
-for _=1,0 do end
-for _=1,0 do end
-for _=1,0 do end
-local oPquK39sbaxL={63,140,152,30,77,95,209,127,179,59,33,91,246,43,157,145,72,119,52,12,72,67,222,180,179,231,113,129,163,14,67,9,122,86,164,7,227,204}
-local gwKEX6m5Bx={}
-for zY6yF5hWPp7u=1,#oLqrgMicI do
-  gwKEX6m5Bx[zY6yF5hWPp7u]=string.char(bit32.bxor(oLqrgMicI[zY6yF5hWPp7u],oPquK39sbaxL[(zY6yF5hWPp7u-1)%#oPquK39sbaxL+1]))
-end
-local e9Y6l6rMXoFP=table.concat(gwKEX6m5Bx)
-local pFcl5IW0zaVyU6u3=tostring(17)
-local oPXlmvxCYwtdWBaA=834
-for _=1,0 do end
-local eiOidSGoXjhNTP,kMtK1Akw3qJxqmu1=loadstring(e9Y6l6rMXoFP)
-if eiOidSGoXjhNTP then
-  eiOidSGoXjhNTP()
-else
-  error("Mnx | Public Enemy : "..tostring(kMtK1Akw3qJxqmu1))
-end
+--[[
+    ================================================================
+    [ SCRIPT INFORMATION ]
+    Project: Custom Script
+    Author: OYB
+    YouTube: https://www.youtube.com/channel/UCAlXXV1Hbvf7WbfXARuVtiQ
+    
+    [ TERMS AND CONDITIONS ]
+    - You ARE allowed to use and modify this script for your own games.
+    - You ARE NOT allowed to re-upload, redistribute, or claim 
+      ownership of this script.
+    - Removing or altering these credits is strictly prohibited.
+    
+    Copyright (c) 2026 OYB. All rights reserved.
+    ================================================================
+]]
+local Config = {
+    -- [1] PlatoBoost Settings
+    ServiceId       = 32730, -- Your PlatoBoost Service ID
+    PlatoSecret     = "d9fcd891-cfa4-44cd-9e03-1ea56f7e4690", -- Your PlatoBoost Secret Key
+
+    -- [2] Anti-Bypass / Global Secret Variable
+    Secret          = "sdflkjfdslks@!#dlfjouqljsxnmcbxvx983274$#@dsflksjdf", -- This makes the script ONLY run from the key script. Even if they copy the original obfuscated script to bypass the key, they won't be able to!
+    
+    -- [3] Scripts & Links
+    MainScriptURL   = "https://raw.githubusercontent.com/dangerdangep-beep/keysystem/refs/heads/main/script.lua", -- The raw URL of your main script
+    
+    -- [4] Social Media Settings (Set to true to show, false to hide)
+    ShowDiscord     = true,
+    DiscordURL      = "https://discord.gg/dQmMtvRTCc",
+    
+    ShowInstagram   = false,
+    InstagramURL    = "https://www.instagram.com/oyb0i/",
+    
+    ShowYoutube     = false,
+    YoutubeURL      = "https://www.youtube.com/channel/UCAlXXV1Hbvf7WbfXARuVtiQ",
+
+    -- [5] File System
+    KeyFileName     = "Mykey.txt", -- The name of the file where the valid key will be saved for auto-login
+
+    -- [6] GUI Management
+    OldGuiName      = "script", -- Name of the old GUI to destroy if it's already open
+    MainGuiName     = "script", -- Name of the main script's GUI to check if it's already executing
+
+    -- [7] Hub Information & UI Text
+    HubName         = "OpleoniHaters", -- The main title shown at the top of the GUI
+    HubDescription  = "OpleoniHaters" -- The text shown below the title
+}
